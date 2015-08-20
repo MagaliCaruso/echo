b78da9a0c14469196ae0870d9ca359850f880226
unknown <magalicaruso@gmail.com>
2015-08-20 18:00:43 -0300

    vamos que se puede

--- src/echo.js
+++ src/echo.js
@@ -3,10 +3,11 @@ angular.module('plugin', ['ngCordova', 'ionic'])
 
     .factory('echo', function ($window) {
 
+        //cordova.exec(function(winParam) {}, function(error) {}, "service", "action", ["firstArgument", "secondArgument", 42, false]);
+
+
         window.echo = function(str, callback) {
-            cordova.exec(callback, function(err) {
-                callback('Nothing to echo.');
-                }, "Echo", "echo", [str]);
+            cordova.exec(callback, function(err) {callback('Nothing to echo.');}, "Echo", "echo", [str] ); 
         };
 
         return 1;
